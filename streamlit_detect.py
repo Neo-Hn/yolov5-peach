2794864a520ab586b713aa0f0cfa864e54a8ca80
KindB <52965101+Neo-Hn@users.noreply.github.com>
2022-11-14 01:53:55 +0800

    Update streamlit_detect.py
    Signed-off-by: KindB <52965101+Neo-Hn@users.noreply.github.com>

--- streamlit_detect.py
+++ streamlit_detect.py
@@ -51,7 +51,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 def run(opt, save_img=False):
     weights, source, imgsz, conf_thres, iou_thres, view_img, save_txt, name = opt.weights, opt.source, opt.imgsz, opt.conf_thres, opt.iou_thres, opt.view_img, opt.save_txt, opt.name
     data = 'data/peach.yaml'
-    dnn, half, vid_stride = False, False, 1
+    dnn, half, vid_stride, visualize = False, False, 1, False
     source = str(source)
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
