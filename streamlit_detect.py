57f121ffb31a444b4e81a5c7f806c6e51f92d5f4
KindB <52965101+Neo-Hn@users.noreply.github.com>
2022-11-14 02:18:20 +0800

    Update streamlit_detect.py
    Signed-off-by: KindB <52965101+Neo-Hn@users.noreply.github.com>

--- streamlit_detect.py
+++ streamlit_detect.py
@@ -52,7 +52,7 @@ def run(opt, save_img=False):
     weights, source, imgsz, conf_thres, iou_thres, view_img, save_txt, name = opt.weights, opt.source, opt.imgsz, opt.conf_thres, opt.iou_thres, opt.view_img, opt.save_txt, opt.name
     data = 'data/peach.yaml'
     dnn, half, vid_stride, visualize, classes = False, False, 1, False, None
-    augment, update, max_det, save_crop, save_conf, agnostic_nms = opt.augment, opt.update, opt.max_det, opt.save_crop, opt.save_conf, opt.agnostic_nms
+    augment, update, max_det, save_crop, save_conf, agnostic_nms = opt.augment, opt.update, 1000, opt.save_crop, opt.save_conf, opt.agnostic_nms
     
     source = str(source)
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
