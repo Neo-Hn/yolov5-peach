bcacf4d6dee6b2f9bad0318ae04cc6c300719337
KindB <52965101+Neo-Hn@users.noreply.github.com>
2022-11-13 23:06:41 +0800

    Update plots.py
    Signed-off-by: KindB <52965101+Neo-Hn@users.noreply.github.com>

--- utils/plots.py
+++ utils/plots.py
@@ -20,9 +20,10 @@ import torch
 from PIL import Image, ImageDraw, ImageFont
 
 from utils import TryExcept, threaded
-from utils.general import (CONFIG_DIR, FONT, LOGGER, check_font, check_requirements, clip_coords, increment_path,
+from utils.general import (CONFIG_DIR, FONT, LOGGER, check_font, check_requirements, clip_boxes, increment_path,
                            is_ascii, xywh2xyxy, xyxy2xywh)
 from utils.metrics import fitness
+from utils.segment.general import scale_image
 
 # 仅对圆形目标生效
 some_labels = ['sports ball', 'broccoli', 'pizza', 'stop sign', 'apple', 'orange', 'donut', 'cake', 'clock', 'peach']
