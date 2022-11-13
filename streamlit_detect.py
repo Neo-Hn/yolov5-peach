c0c07d2ad663a0876c23b580188cd9e91c4b8283
KindB <52965101+Neo-Hn@users.noreply.github.com>
2022-11-14 00:28:04 +0800

    Update streamlit_detect.py
    Signed-off-by: KindB <52965101+Neo-Hn@users.noreply.github.com>

--- streamlit_detect.py
+++ streamlit_detect.py
@@ -241,7 +241,7 @@ if __name__ == "__main__":
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     opt = parser.parse_args()
     
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    opt.imgsz = (640, 640)  # expand
     check_requirements(exclude=('tensorboard', 'thop'))
     
     run(**vars(opt))
