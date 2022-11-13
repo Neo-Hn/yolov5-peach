3cec0afa4b16da6caf1018ea33be3827f7825601
KindB <52965101+Neo-Hn@users.noreply.github.com>
2022-11-14 00:51:31 +0800

    Update streamlit_detect.py
    Signed-off-by: KindB <52965101+Neo-Hn@users.noreply.github.com>

--- streamlit_detect.py
+++ streamlit_detect.py
@@ -48,34 +48,8 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
-def run(
-        weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
-        source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-):
+def run(opt, save_img=False):
+    weights, source, imgsz, conf_thres, iou_thres, view_img, save_txt = opt.weights, opt.source, opt.imgsz, opt.conf_thres, opt.iou_thres, opt.view_img, opt.save_txt
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
@@ -244,4 +218,4 @@ if __name__ == "__main__":
     opt.imgsz = (opt.imgse, opt.imgsz)  # expand
     check_requirements(exclude=('tensorboard', 'thop'))
     
-    run(**vars(opt))
+    run(opt)
